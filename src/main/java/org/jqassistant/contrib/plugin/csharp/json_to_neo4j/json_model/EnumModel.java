8782113d164126a1d3935c77c47177a0b4afe8fd
William Thimm <W.Thimm@kontext-e.de>
2023-10-24 13:37:44 +0200

    Added TypeModel from which Class, enum and Interface inherit

--- src/main/java/org/jqassistant/contrib/plugin/csharp/json_to_neo4j/json_model/EnumModel.java
+++ src/main/java/org/jqassistant/contrib/plugin/csharp/json_to_neo4j/json_model/EnumModel.java
@@ -2,30 +2,17 @@ package org.jqassistant.contrib.plugin.csharp.json_to_neo4j.json_model;
 
 import lombok.Getter;
 import lombok.Setter;
-import net.sourceforge.plantuml.json.Json;
 
 import java.util.List;
 
 @Setter
 @Getter
-public class EnumModel implements JsonModel {
-
-    private String name;
-
-    private String fqn;
-
-    private String md5;
+public class EnumModel extends TypeModel implements JsonModel {
 
     private List<EnumMemberModel> members;
 
-    private int firstLineNumber;
-
-    private int lastLineNumber;
-
-    private int effectiveLineCount;
-
     @Override
     public String getKey() {
-        return fqn;
+        return super.getFqn();
     }
 }
