de315bb5216f646b554b59c93c89c954a53321ea
William Thimm <W.Thimm@kontext-e.de>
2024-01-03 14:25:32 +0100

    Refactored MemberAnalyzer and added tests

---
+++ src/test/java/org/jqassistant/contrib/plugin/csharp/json_to_neo4j/MemberAnalyzerTest.java
@@ -0,0 +1,137 @@
+package org.jqassistant.contrib.plugin.csharp.json_to_neo4j;
+
+import com.buschmais.jqassistant.core.store.api.Store;
+import org.jqassistant.contrib.plugin.csharp.json_to_neo4j.caches.FieldCache;
+import org.jqassistant.contrib.plugin.csharp.json_to_neo4j.caches.TypeCache;
+import org.jqassistant.contrib.plugin.csharp.json_to_neo4j.json_model.ClassModel;
+import org.jqassistant.contrib.plugin.csharp.json_to_neo4j.json_model.FieldModel;
+import org.jqassistant.contrib.plugin.csharp.model.FieldDescriptor;
+import org.jqassistant.contrib.plugin.csharp.model.MemberDescriptor;
+import org.jqassistant.contrib.plugin.csharp.model.PrimitiveValueDescriptor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class MemberAnalyzerTest {
+
+    Store store;
+    FieldCache fieldCache;
+    TypeCache typeCache;
+    private MemberAnalyzer memberAnalyzer;
+
+    @BeforeEach
+    void setup(){
+        store = mock();
+        fieldCache = mock();
+        typeCache = mock();
+        memberAnalyzer = new MemberAnalyzer(store, fieldCache, typeCache);
+    }
+
+    @Test
+    void testCreateSingleField(){
+        ClassModel classModel = createClassModel("Class", createFields(1));
+        when(fieldCache.create(any())).thenReturn(new FieldDescriptorImpl());
+        when(typeCache.findOrCreate(any())).thenReturn(new TypeDescriptorImpl("TypeOfField"));
+
+        ClassDescriptorImpl containingClass = new ClassDescriptorImpl("ContainingClass");
+        when(typeCache.findTypeByRelativePath(any(), any())).thenReturn(Optional.of(containingClass));
+        when(store.create(PrimitiveValueDescriptor.class)).thenReturn(new PrimitiveValueDescriptorImpl());
+
+        memberAnalyzer.createFields(classModel, "Relative.Path");
+
+        verify(store, never()).create(PrimitiveValueDescriptor.class);
+        verify(fieldCache).create("fqn.Field0");
+        assertThat(containingClass.getDeclaredMembers().size()).isEqualTo(1);
+        MemberDescriptor memberDescriptor = containingClass.getDeclaredMembers().get(0);
+        assertThat(memberDescriptor).isInstanceOfAny(FieldDescriptor.class);
+        FieldDescriptor fieldDescriptor = (FieldDescriptor) memberDescriptor;
+        assertFieldDescriptorHasBeenFilled(fieldDescriptor);
+        assertThat(fieldDescriptor.getValue()).isNull();
+    }
+
+    @Test
+    void testCreateNoFields(){
+        ClassModel classModel = createClassModel("Class", createFields(0));
+        ClassDescriptorImpl containingClass = new ClassDescriptorImpl("ContainingClass");
+        when(typeCache.findTypeByRelativePath(any(), any())).thenReturn(Optional.of(containingClass));
+
+        memberAnalyzer.createFields(classModel, "Relative.Path");
+
+        verify(store, never()).create(any());
+        assertThat(containingClass.getDeclaredMembers().isEmpty()).isTrue();
+    }
+
+    @Test
+    void testCreateFieldWithDefaultValue(){
+        List<FieldModel> fields = createFields(1);
+        fields.get(0).setConstantValue("ConstantValue");
+        ClassModel classModel = createClassModel("Class", fields);
+        when(fieldCache.create(any())).thenReturn(new FieldDescriptorImpl());
+        when(typeCache.findOrCreate(any())).thenReturn(new TypeDescriptorImpl("TypeOfField"));
+
+        ClassDescriptorImpl containingClass = new ClassDescriptorImpl("ContainingClass");
+        when(typeCache.findTypeByRelativePath(any(), any())).thenReturn(Optional.of(containingClass));
+        when(store.create(PrimitiveValueDescriptor.class)).thenReturn(new PrimitiveValueDescriptorImpl());
+
+        memberAnalyzer.createFields(classModel, "Relative.Path");
+
+        verify(store).create(PrimitiveValueDescriptor.class);
+        verify(fieldCache).create("fqn.Field0");
+        assertThat(containingClass.getDeclaredMembers().size()).isEqualTo(1);
+        MemberDescriptor memberDescriptor = containingClass.getDeclaredMembers().get(0);
+        assertThat(memberDescriptor).isInstanceOfAny(FieldDescriptor.class);
+        FieldDescriptor fieldDescriptor = (FieldDescriptor) memberDescriptor;
+        assertFieldDescriptorHasBeenFilled(fieldDescriptor);
+
+        assertThat(fieldDescriptor.getValue()).isInstanceOfAny(PrimitiveValueDescriptor.class);
+        assertThat(fieldDescriptor.getValue().getValue()).isEqualTo("ConstantValue");
+    }
+
+    private static void assertFieldDescriptorHasBeenFilled(FieldDescriptor fieldDescriptor) {
+        assertThat(fieldDescriptor.getFullQualifiedName()).isNotNull();
+        assertThat(fieldDescriptor.getName()).isNotNull();
+        assertThat(fieldDescriptor.getVisibility()).isNotNull();
+        assertThat(fieldDescriptor.isVolatile()).isTrue();
+        assertThat(fieldDescriptor.isSealed()).isTrue();
+        assertThat(fieldDescriptor.isStatic()).isTrue();
+        assertThat(fieldDescriptor.getType().getName()).isEqualTo("TypeOfField");
+    }
+
+    private ClassModel createClassModel(String name, List<FieldModel> fields){
+        ClassModel classModel = new ClassModel();
+        classModel.setName(name);
+        classModel.setFields(fields);
+
+        return classModel;
+    }
+
+    private List<FieldModel> createFields(int amount) {
+        List<FieldModel> fieldModels = new ArrayList<>();
+        for (int i = 0; i<amount; i++){
+            FieldModel fieldModel = new FieldModel();
+            fillFieldModel(fieldModel, i);
+            fieldModels.add(fieldModel);
+        }
+        return fieldModels;
+    }
+
+    private void fillFieldModel(FieldModel fieldModel, int seed) {
+        fieldModel.setFqn("fqn.Field"+seed);
+        fieldModel.setName("Field"+seed);
+        fieldModel.setAccessibility("private");
+        fieldModel.setVolatileKeyword(true);
+        fieldModel.setSealed(true);
+        fieldModel.setStaticKeyword(true);
+    }
+
+}
--- src/test/java/org/jqassistant/contrib/plugin/csharp/json_to_neo4j/TestImplementations.java
+++ src/test/java/org/jqassistant/contrib/plugin/csharp/json_to_neo4j/TestImplementations.java
@@ -14,6 +14,7 @@ import org.jqassistant.contrib.plugin.csharp.model.MemberDescriptor;
 import org.jqassistant.contrib.plugin.csharp.model.MethodDescriptor;
 import org.jqassistant.contrib.plugin.csharp.model.NamespaceDescriptor;
 import org.jqassistant.contrib.plugin.csharp.model.ParameterDescriptor;
+import org.jqassistant.contrib.plugin.csharp.model.PrimitiveValueDescriptor;
 import org.jqassistant.contrib.plugin.csharp.model.TypeDescriptor;
 import org.jqassistant.contrib.plugin.csharp.model.UsesNamespaceDescriptor;
 
@@ -825,3 +826,245 @@ class MethodDescriptorImpl implements MethodDescriptor {
 }
 
 class ConstructorDescriptorImpl extends MethodDescriptorImpl implements ConstructorDescriptor {}
+
+class PrimitiveValueDescriptorImpl implements PrimitiveValueDescriptor{
+
+    private String name;
+    private TypeDescriptor type;
+    private Object value;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public TypeDescriptor getType() {
+        return type;
+    }
+
+    @Override
+    public void setType(TypeDescriptor type) {
+        this.type = type;
+    }
+
+    @Override
+    public Object getValue() {
+        return value;
+    }
+
+    @Override
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    @Override
+    public <I> I getId() {
+        return null;
+    }
+
+    @Override
+    public <T> T as(Class<T> aClass) {
+        return null;
+    }
+
+    @Override
+    public <D> D getDelegate() {
+        return null;
+    }
+}
+
+class FieldDescriptorImpl implements FieldDescriptor{
+
+    private String visibility;
+    private boolean isStatic;
+    private boolean readonly;
+    private boolean isConst;
+    private boolean isSealed;
+    private boolean isNew;
+    private boolean isExtern;
+    private boolean isOverride;
+    private boolean isVirtual;
+
+
+    private String signature;
+    private Boolean isTransient;
+    private Boolean isVolatile;
+    private String FullQualifiedName;
+    private String name;
+    private TypeDescriptor type;
+    private PrimitiveValueDescriptor value;
+    private TypeDescriptor declaringType;
+
+    public TypeDescriptor getDeclaringType() {
+        return declaringType;
+    }
+
+    @Override
+    public PrimitiveValueDescriptor getValue() {
+        return value;
+    }
+
+    @Override
+    public void setValue(PrimitiveValueDescriptor value) {
+        this.value = value;
+    }
+
+    @Override
+    public String getSignature() {
+        return signature;
+    }
+
+    @Override
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public Boolean isTransient() {
+        return isTransient;
+    }
+
+    @Override
+    public void setTransient(Boolean aTransient) {
+        isTransient = aTransient;
+    }
+
+    public Boolean isVolatile() {
+        return isVolatile;
+    }
+
+    @Override
+    public void setVolatile(Boolean aVolatile) {
+        isVolatile = aVolatile;
+    }
+
+    public String getFullQualifiedName() {
+        return FullQualifiedName;
+    }
+
+    public void setFullQualifiedName(String fullQualifiedName) {
+        FullQualifiedName = fullQualifiedName;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public TypeDescriptor getType() {
+        return type;
+    }
+
+    @Override
+    public void setType(TypeDescriptor type) {
+        this.type = type;
+    }
+
+    @Override
+    public String getVisibility() {
+        return visibility;
+    }
+
+    @Override
+    public void setVisibility(String visibility) {
+        this.visibility = visibility;
+    }
+
+    @Override
+    public Boolean isStatic() {
+        return isStatic;
+    }
+
+    public void setStatic(Boolean aStatic) {
+        isStatic = aStatic;
+    }
+
+    @Override
+    public Boolean isReadonly() {
+        return readonly;
+    }
+
+    public void setReadonly(Boolean readonly) {
+        this.readonly = readonly;
+    }
+
+    @Override
+    public Boolean isConst() {
+        return isConst;
+    }
+
+    public void setConst(Boolean aConst) {
+        isConst = aConst;
+    }
+
+    @Override
+    public Boolean isSealed() {
+        return isSealed;
+    }
+
+    public void setSealed(Boolean sealed) {
+        isSealed = sealed;
+    }
+
+    @Override
+    public Boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(Boolean aNew) {
+        isNew = aNew;
+    }
+
+    @Override
+    public Boolean isExtern() {
+        return isExtern;
+    }
+
+    public void setExtern(Boolean extern) {
+        isExtern = extern;
+    }
+
+    @Override
+    public Boolean isOverride() {
+        return isOverride;
+    }
+
+    public void setOverride(Boolean override) {
+        isOverride = override;
+    }
+
+    @Override
+    public Boolean isVirtual() {
+        return isVirtual;
+    }
+
+    public void setVirtual(Boolean virtual) {
+        isVirtual = virtual;
+    }
+
+    @Override
+    public <I> I getId() {
+        return null;
+    }
+
+    @Override
+    public <T> T as(Class<T> aClass) {
+        return null;
+    }
+
+    @Override
+    public <D> D getDelegate() {
+        return null;
+    }
+}
