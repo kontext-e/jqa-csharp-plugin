d0db7bb7a5b6f43206aa5cb3e36fe6f7e72e0fbd
William Thimm <W.Thimm@kontext-e.de>
2024-01-17 13:58:50 +0100

    Reworked IntegrationTests

--- src/test/java/org/jqassistant/contrib/plugin/csharp/scanner/ScannerTestIT.java
+++ src/test/java/org/jqassistant/contrib/plugin/csharp/scanner/ScannerTestIT.java
@@ -4,15 +4,12 @@ import com.buschmais.jqassistant.core.scanner.api.DefaultScope;
 import com.buschmais.jqassistant.plugin.common.test.AbstractPluginIT;
 import org.jqassistant.contrib.plugin.csharp.model.ClassDescriptor;
 import org.jqassistant.contrib.plugin.csharp.model.InterfaceTypeDescriptor;
-import org.jqassistant.contrib.plugin.csharp.model.MethodDescriptor;
 import org.jqassistant.contrib.plugin.csharp.model.NamespaceDescriptor;
-import org.jqassistant.contrib.plugin.csharp.model.PropertyDescriptor;
 import org.jqassistant.contrib.plugin.csharp.model.UsesNamespaceDescriptor;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,7 +28,6 @@ public class ScannerTestIT extends AbstractPluginIT {
         testClasses();
         testBaseTypes();
         testInterfaces();
-        testProperties();
 
         store.commitTransaction();
     }
@@ -121,84 +117,6 @@ public class ScannerTestIT extends AbstractPluginIT {
 
         assertThat(interfaceTypeDescriptor.getInterfaces()).hasSize(1);
         assertThat(interfaceTypeDescriptor.getInterfaces().get(0).getName()).isEqualTo("ParentInterface");
-    }
-
-    private void testProperties() {
-        List<Map<String, Object>> propertyDescriptorList = query("Match (p:Property) Return p").getRows();
-        assertThat(propertyDescriptorList.size()).isEqualTo(12);
-
-        testPropertyInlineGetSetDefinition();
-        testPropertyPrivate();
-        testPropertyStatic();
-        testPropertyInlineGetSetWithVisibility();
-        testPropertyExpressionBodiedGetSet();
-        testPropertyInit();
-        testImplicitAccessModifier();
-    }
-
-    private void testPropertyStatic() {
-        List<Map<String, Object>> property7 = query("Match (p:Property {name: \"Property8\"}) Return p").getRows();
-        assertThat(property7.size()).isEqualTo(1);
-        assertThat(((PropertyDescriptor)property7.get(0).get("p")).isStatic()).isEqualTo(true);
-    }
-
-    private void testPropertyPrivate() {
-        List<Map<String, Object>> property7 = query("Match (p:Property {name: \"Property7\"}) Return p").getRows();
-        assertThat(property7.size()).isEqualTo(1);
-        assertThat(((PropertyDescriptor)property7.get(0).get("p")).getAccessibility()).isEqualTo("Private");
-    }
-
-    private void testPropertyInlineGetSetDefinition() {
-        List<Map<String, Object>> property1 = query("Match (p:Property {name: \"Property1\"}) Return p").getRows();
-        assertThat(property1.size()).isEqualTo(1);
-
-        List<Object> accessors = query("Match r=((p:Property {name: \"Property1\"})-[:DECLARES]->(m)) Return m").getColumn("m");
-        assertThat(accessors.size()).isEqualTo(2);
-        assertThat(((MethodDescriptor) accessors.get(0)).getAccessibility()).isEqualTo("Public");
-        assertThat(((MethodDescriptor) accessors.get(1)).getAccessibility()).isEqualTo("Public");
-    }
-
-    private void testPropertyInlineGetSetWithVisibility() {
-        List<Map<String, Object>> property1 = query("Match (p:Property {name: \"Property2\"}) Return p").getRows();
-        assertThat(property1.size()).isEqualTo(1);
-
-        List<Object> accessors = query("Match r=((p:Property {name: \"Property2\"})-[:DECLARES]->(m)) Return m").getColumn("m");
-        assertThat(accessors.size()).isEqualTo(2);
-        for (Object accessor : accessors){
-            if (((MethodDescriptor) accessor).getFullQualifiedName().endsWith("set")){
-                assertThat(((MethodDescriptor) accessor).getAccessibility()).isEqualTo("Private");
-            } else {
-                assertThat(((MethodDescriptor) accessor).getAccessibility()).isEqualTo("Public");
-            }
-        }
-    }
-
-    private void testPropertyExpressionBodiedGetSet() {
-        List<Map<String, Object>> property1 = query("Match (p:Property {name: \"Property3\"}) Return p").getRows();
-        assertThat(property1.size()).isEqualTo(1);
-
-        List<Object> accessors = query("Match r=((p:Property {name: \"Property3\"})-[:DECLARES]->(m)) Return m").getColumn("m");
-        assertThat(accessors.size()).isEqualTo(1);
-        assertThat(((MethodDescriptor) accessors.get(0)).getAccessibility()).isEqualTo("Public");
-    }
-
-    private void testPropertyInit() {
-        boolean initDetected = false;
-        List<Object> accessors = query("Match r=((p:Property {name: \"Property6\"})-[:DECLARES]->(m)) Return m").getColumn("m");
-        assertThat(accessors.size()).isEqualTo(2);
-        for (Object accessor : accessors){
-            if (((MethodDescriptor)accessor).getFullQualifiedName().endsWith("init")){
-                initDetected = true;
-                break;
-            }
-        }
-        assertThat(initDetected).isTrue();
-    }
-
-    private void testImplicitAccessModifier(){
-        List<Map<String, Object>> property10 = query("Match (p:Property {name: \"Property10\"}) Return p").getRows();
-        assertThat(property10.size()).isEqualTo(1);
-        assertThat(((PropertyDescriptor)property10.get(0).get("p")).getAccessibility()).isEqualTo("Private");
     }
 
 }
---
+++ src/test/resources/scanner/test-csharp-project/CSharpJqAssistantTestProject/Project1/Methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Project_1;
+
+public class Methods
+{
+    public Methods(){}
+    
+    void ImplicitlyPrivateMethod(){}
+    public void PublicMethod(){}
+    private void PrivateMethod(){}
+    protected void ProtectedMethod(){}
+    private protected void PrivateProtectedMethod(){}
+    internal void InternalMethod(){}
+    protected internal void ProtectedInternalMethod(){}
+    
+    public void MethodWithNoArguments(){}
+    public void MethodWithDefaultArguments(Methods methods, int number = 2){}
+
+    public void MethodWithNoReturnType(){}
+    public int MethodWithSimpleReturnType() { return 3; }
+    public Methods MethodWithTypeReturnType() { return new Methods(); }
+    
+    public void MethodWithLocalMethod() { void LocalMethod(){} }
+
+    public void ExpressionMethod(string text) => Console.WriteLine(text);
+}
