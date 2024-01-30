41d8fd60fd67a993be554e170c4124c08d56f444
William Thimm <W.Thimm@kontext-e.de>
2024-01-30 15:37:44 +0100

    Improved test Data for Partiality Tests

---
+++ src/test/resources/scanner/test-csharp-project/CSharpJqAssistantTestProject/Project1/Partiality/PartialClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Project_1.Partiality
+{
+    public partial class PartialClass
+    {
+        private partial int PartialMethod() { return 0; }
+    }
+    
+    public partial class PartialClass
+    {
+        private partial int PartialMethod();
+        partial void UnimplementedMethod();
+    }
+}
