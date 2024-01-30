e11d794aef894920024f50ddba34d1d0f90af8e8
William Thimm <W.Thimm@kontext-e.de>
2024-01-30 17:11:07 +0100

    Added Integration Tests for DependencyAnalyzer

---
+++ src/test/resources/scanner/test-csharp-project/CSharpJqAssistantTestProject/Project1/TypeDependencies/Inheritance.cs
@@ -0,0 +1,9 @@
+﻿namespace Project_1.TypeDependencies;
+
+public abstract class ParentClass { }
+public class FirstChildClass : ParentClass { }
+public class SecondChildClass : ParentClass { }
+
+public record ParentRecord {}
+public record FirstChildRecord : ParentRecord {}
+public record SecondChildRecord : ParentRecord {}
