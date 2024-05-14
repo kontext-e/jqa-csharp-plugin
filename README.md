592f3dccd499f32889919fcb9874e8388d1a1dd9
WilliamThimm <William.Thimm@gmail.com>
2024-05-14 12:20:00 +0200

    Updated README.md and increased version

--- README.md
+++ README.md
@@ -9,7 +9,23 @@ It enables jQAssistant to scan and to analyze **C#** files.
 
 Download the jQAssistant command line tool for your system: [jQAssistant - Get Started](https://jqassistant.org/get-started/).
 
+## Installation
+### Installation through jQAssistant
+
+Starting with jQAssistant 1.12.0 it is possible to declare the plugins in the .jqassistant.yaml file (see: [jQAssistant User Manual](https://jqassistant.github.io/jqassistant/doc/1.12.2/manual/index.html#_yaml_files))
+
+Simply add the following to the plugins section of the config
+```yaml
+  - group-id: de.kontext-e.jqassistant.plugin
+    artifact-id: jqassistant.plugin.csharp
+    version: 0.2.2
+```
+
+### Manual Installation
+
 Next download the latest version from the release tab. Put the `jqa-csharp-plugin-*.jar` into the plugins folder of the jQAssistant commandline tool.
+
+## Usage
 
 Now scan your C# project:
 
--- src/main/java/org/jqassistant/contrib/plugin/csharp/csharp_to_json/CSharpToJsonToolFolders.java
+++ src/main/java/org/jqassistant/contrib/plugin/csharp/csharp_to_json/CSharpToJsonToolFolders.java
@@ -3,6 +3,8 @@ package org.jqassistant.contrib.plugin.csharp.csharp_to_json;
 import net.harawata.appdirs.AppDirs;
 import net.harawata.appdirs.AppDirsFactory;
 
+import static org.jqassistant.contrib.plugin.csharp.csharp_to_json.CSharpToJsonToolManager.CSHARP_TO_JSON_TOOL_VERSION;
+
 public class CSharpToJsonToolFolders {
 
     private final AppDirs appDirs;
@@ -13,11 +15,11 @@ public class CSharpToJsonToolFolders {
 
     public String buildToolPath() {
 
-        return appDirs.getUserDataDir("csharp-to-json-converter", "0.2.0", "jqassistant-contrib");
+        return appDirs.getUserDataDir("csharp-to-json-converter", CSHARP_TO_JSON_TOOL_VERSION, "jqassistant-contrib");
     }
 
     public String buildPluginDataPath() {
 
-        return appDirs.getUserDataDir("csharp-jqassistant-plugin", "0.2.0", "jqassistant-contrib");
+        return appDirs.getUserDataDir("csharp-jqassistant-plugin", CSHARP_TO_JSON_TOOL_VERSION, "jqassistant-contrib");
     }
 }
